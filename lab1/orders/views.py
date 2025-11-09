5506ff2701154056045d16b126d3e54a91af4e31
maxpyatkovskyi699 <maxlegend1901@gmail.com>
2025-11-09 23:10:03 +0200

    add lab7: API with Token-authorization

--- lab1/orders/views.py
+++ lab1/orders/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Ласкаво просимо до розділу товарів!")
+    return HttpResponse("Ласкаво просимо до розділу замовлень!")
